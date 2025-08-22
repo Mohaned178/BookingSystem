a79bc4ba3f4d9f5324ce23b411e21f47d91bd7e3
Mohanad <mohanedhesham644@gmail.com>
2025-08-23 02:35:53 +0300

    Added UserService, IUserService and migrations

---
+++ IUserService.cs
@@ -0,0 +1,19 @@
+﻿using BookingSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore
+{
+    public interface IUserService
+    {
+        void CreateUser(string name, string email, string role);
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+        void UpdateUser(int id, string newName, string newEmail, string newRole);
+        void DeleteUser(int id);
+
+    }
+}
